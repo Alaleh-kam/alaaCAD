7d3d1e5d3c773107bb0b505827c6e1135656a522
Alaleh-kam <61510514+Alaleh-kam@users.noreply.github.com>
2022-03-08 14:22:22 +0100

    return value of input() no more string

--- shapes_interface.py
+++ shapes_interface.py
@@ -1,8 +1,8 @@
 def getShape():
     print("The only available shape is currently rectangle")
     newShape={}
-    newShape["width"]=float(input("width:"))
-    newShape["length"]=float(input("length:"))
-    newShape["upper_left_x"]=float(input("upper left x:"))
-    newShape["upper_left_y"]=float(input("upper left y:"))
+    newShape["width"]=input("width:")
+    newShape["length"]=input("length:")
+    newShape["upper_left_x"]=input("upper left x:")
+    newShape["upper_left_y"]=input("upper left y:")
     return newShape

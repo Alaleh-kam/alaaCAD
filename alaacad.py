95c868c56e6880730a2fd97190880df15a6cf7e5
Alaleh-kam <61510514+Alaleh-kam@users.noreply.github.com>
2022-04-04 11:55:47 +0200

    add triangle to the list

--- alaacad.py
+++ alaacad.py
@@ -5,9 +5,10 @@ shapes=[]
 menu_choice=''
 while menu_choice!=0:
 
-    print("1) add shape")
-    print("2) list shapes")
-    print("3) draw shapes")
+    print("1) add rectangle")
+    print("2) add triangle")
+    print("3) list shapes")
+    print("4) draw shapes")
     print("0) exit")
 
     menu_choice=input("Select from the menu:")
@@ -15,8 +16,10 @@ while menu_choice!=0:
     if menu_choice==1:
         shapes.append(shi.getShape())
     elif menu_choice==2:
-        shi.printList(shapes)
+        shapes.append(shi.getShape())
     elif menu_choice==3:
+        shi.printList(shapes)
+    elif menu_choice==4:
         shg.drawShape(shapes)
 
 
